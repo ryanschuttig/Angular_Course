09c5079872d8da70592f9abf31094428214ff590
ryanschuttig <ryanschuttig@gmail.com>
2022-07-19 10:42:01 -0400

    created recipe model and added to recipe component

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://static01.nyt.com/images/2022/05/05/dining/rm-tajin-grilled-chicken-copy/merlin_206068182_ffd1a522-033b-4519-969f-1990ecb8a8f0-videoSixteenByNine3000.jpg')
+  ];
 
   constructor() { }
 
