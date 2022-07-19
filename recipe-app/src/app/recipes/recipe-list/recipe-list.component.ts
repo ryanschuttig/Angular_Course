737dfb6275e926533b774f40002fb42178408723
ryanschuttig <ryanschuttig@gmail.com>
2022-07-19 11:02:28 -0400

    display recipe details

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,16 +4,18 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://static01.nyt.com/images/2022/05/05/dining/rm-tajin-grilled-chicken-copy/merlin_206068182_ffd1a522-033b-4519-969f-1990ecb8a8f0-videoSixteenByNine3000.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://media.istockphoto.com/vectors/recipe-book-hand-drawn-cover-vector-illustration-vector-id1185879263?k=20&m=1185879263&s=612x612&w=0&h=Qiw3sY0LiWG4IIKcKQI9fAwAxR81xLmbhRpYpgt3S8I='
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
